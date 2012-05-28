0f136728e5513c3c5bfd8882253f3cb0c5a12635
Ariel Lipschutz <ariellipschutz@gmail.com>
2012-05-28 11:44:04 -0300

    el sitio

---
+++ Sao/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["Usuario"] != null){
+                return View();
+              } else {
+                 return RedirectToAction("Login", "Alumnos");
+              }
+            
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sao.MvcApplication" Language="C#" %>
